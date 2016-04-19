d2a03452ab4147744aa8b7d321f5218bc0ddacd0
teh-BMW <fattolo170@gmail.com>
2016-04-19 18:26:16 +0200

    test commit

--- HoA/src/Movement.java
+++ HoA/src/Movement.java
@@ -4,6 +4,6 @@
 public class Movement {
 
     public static void main(String [] args){
-        System.out.println("HelLo world!");
+        System.out.println("HelLo world! I am alive");
     }
 }
