c253faf92ddd6bc2b098da7e746bede5a9a8271b
Lorenzo Spoleti <spolel@usi.ch>
2016-04-19 18:28:26 +0200

    ok

--- HoA/src/Movement.java
+++ HoA/src/Movement.java
@@ -3,7 +3,8 @@
  */
 public class Movement {
 
-    public static void main(String [] args){
+    public static void main(String [] args)
+    {
         System.out.println("HelLo world!");
     }
 }
