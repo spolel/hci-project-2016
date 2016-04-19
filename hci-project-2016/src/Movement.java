f915de0c5a6961f8b7874987e0ee84107ba1c4c2
teh-BMW <fattolo170@gmail.com>
2016-04-19 16:53:36 +0200

    project stub
    added project stub

---
+++ hci-project-2016/src/Movement.java
@@ -0,0 +1,19 @@
+/**
+ * Created by BMW on 19/04/2016.
+ */
+public class Movement {
+
+    private int x;
+
+    public Movement(){
+
+        x=1;
+    }
+
+    public int test(int value){
+        if(value == 0){
+            return 0;
+        }
+        else return value;
+    }
+}
