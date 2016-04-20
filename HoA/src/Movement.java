e84e74b161ac70ee9daf302fef7a8180d58e7bf9
teh-BMW <fattolo170@gmail.com>
2016-04-20 10:52:54 +0200

    test commit without xmls

--- HoA/src/Movement.java
+++ HoA/src/Movement.java
@@ -4,6 +4,6 @@
 public class Movement {
 
     public static void main(String [] args) {
-        System.out.println("HelLo world! I am alive");
+        System.out.println("HelLo world! I am alive, test without xmls");
     }
 }
