3206a56b364b6570f41e972b467f7f5bdeacf4a9
shughi <shugar94@hotmail.com>
2016-04-20 13:44:58 +0200

    my test

--- HoA/src/Movement.java
+++ HoA/src/Movement.java
@@ -4,6 +4,6 @@
 public class Movement {
 
     public static void main(String [] args) {
-        System.out.println("HelLo world! I am alive, test without xmls; github is deadded");
+        System.out.println("HelLo world! I am alive, test without xmls; github is deadded, and boh this is my comment shughi hue");
     }
 }
