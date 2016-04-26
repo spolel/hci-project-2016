5743c472cfe0b2862645a7c95c358f87d62e353e
Lorenzo Spoleti <spolel@usi.ch>
2016-04-26 14:27:42 +0200

    libgdx project setup

--- HoA/src/Movement.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by BMW on 19/04/2016.
- */
-public class Movement {
-
-    public static void main(String [] args) {
-        System.out.println("HelLo world! I am alive, test without xmls; github is deadded, and boh this is my comment shughi hue");
-    }
-}
