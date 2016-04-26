c6c26857052a5ed7fa1fe06949c5650ed8eb0d63
teh-BMW <fattolo170@gmail.com>
2016-04-26 15:32:56 +0200

    refactored the main project to a more widely used format; added PlayScreen class to separate the rendering

---
+++ HoA/core/src/com/hoa/game/screens/PlayScreen.java
@@ -0,0 +1,62 @@
+package com.hoa.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.hoa.game.HoA;
+
+/**
+ * Created by BMW on 26/04/2016.
+ */
+public class PlayScreen implements Screen {
+
+    private HoA game;
+    Texture texture;
+
+    public PlayScreen(HoA game){
+        this.game = game;
+        this.texture = new Texture("badlogic.jpg");
+
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
