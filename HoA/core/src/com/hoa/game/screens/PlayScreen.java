e2e9e401b0f7e6f24d8d146cca6d4dd5c94050de
teh-BMW <fattolo170@gmail.com>
2016-04-27 15:59:51 +0200

    Added proper viewport, now it stretches while keeping aspect ratio.

--- HoA/core/src/com/hoa/game/screens/PlayScreen.java
+++ HoA/core/src/com/hoa/game/screens/PlayScreen.java
@@ -3,7 +3,12 @@ package com.hoa.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.hoa.game.HoA;
 
 /**
@@ -13,10 +18,15 @@ public class PlayScreen implements Screen {
 
     private HoA game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(HoA game){
         this.game = game;
         this.texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 600, gamecam);
+
 
     }
     @Override
@@ -28,6 +38,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -37,6 +48,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        gamePort.update(width, height);
 
     }
 
