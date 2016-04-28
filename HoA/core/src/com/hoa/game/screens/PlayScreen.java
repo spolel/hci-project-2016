bf46c362d98703df75546bb50500aee3df7a3a42
teh-BMW <fattolo170@gmail.com>
2016-04-28 14:33:20 +0200

    working on playscreen

--- HoA/core/src/com/hoa/game/screens/PlayScreen.java
+++ HoA/core/src/com/hoa/game/screens/PlayScreen.java
@@ -5,11 +5,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.hoa.game.HoA;
+import com.hoa.game.Scenes.Hud;
 
 /**
  * Created by BMW on 26/04/2016.
@@ -17,18 +21,37 @@ import com.hoa.game.HoA;
 public class PlayScreen implements Screen {
 
     private HoA game;
-    Texture texture;
+
+    // Current game camera & screen display
     private OrthographicCamera gamecam;
     private Viewport gamePort;
+    private Hud hud;
+
+    //map loader and renderer
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(HoA game){
         this.game = game;
-        this.texture = new Texture("badlogic.jpg");
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(HoA.screenWidth, HoA.screenHeight, gamecam);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("Maps/test_map.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getScreenWidth()/2, gamePort.getScreenHeight()/2, 0);
+        hud = new Hud(game.batch);
+
 
 
     }
+
+
+    public void update(float dt){
+       // handleInput();
+    }
+
+
     @Override
     public void show() {
 
@@ -38,10 +61,9 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gamecam.combined);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
 
 
     }
