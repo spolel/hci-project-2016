fa8fe901866588abe4497f1dbe4b4139dc232201
Lorenzo Spoleti <spolel@usi.ch>
2016-04-28 15:57:28 +0200

    Collisions

--- HoA/core/src/com/hoa/game/screens/PlayScreen.java
+++ HoA/core/src/com/hoa/game/screens/PlayScreen.java
@@ -6,17 +6,22 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.hoa.game.HoA;
 import com.hoa.game.Scenes.Hud;
-
+import com.hoa.game.Sprites.Player;
+import com.badlogic.gdx.math.Vector2;
 import static com.badlogic.gdx.Input.Buttons.RIGHT;
+import com.badlogic.gdx.math.Rectangle;
 
 /**
  * Created by BMW on 26/04/2016.
@@ -36,6 +41,11 @@ public class PlayScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+    //box2d
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
+
     public PlayScreen(HoA game){
         this.game = game;
         gamecam = new OrthographicCamera();
@@ -48,8 +58,29 @@ public class PlayScreen implements Screen {
         gamecam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2, 0);
 
 
+        world = new World(new Vector2(0,0), true);
+        b2dr = new Box2DDebugRenderer();
 
+        /** for now here but later on its own class*/
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
 
+        /** cycling through all objects from the collision layer of the TILED map */
+        /** Here we set the collision layer for collision*/
+        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
     }
 
 
@@ -73,6 +104,9 @@ public class PlayScreen implements Screen {
     public void update(float dt){
         handleInput(dt);
 
+        /** 60 times a second calculate the physics*/
+        world.step(1/60f, 6 ,2);
+
         gamecam.update();
         renderer.setView(gamecam);
     }
@@ -93,7 +127,8 @@ public class PlayScreen implements Screen {
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
-
+        // render the Box2d lines
+        b2dr.render(world,gamecam.combined);
     }
 
     @Override
