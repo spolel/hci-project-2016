e4c66da90414d68080f292399a34e739085bb6b2
teh-BMW <fattolo170@gmail.com>
2016-04-28 15:07:38 +0200

    map displayed, camera moves correctly

--- HoA/core/src/com/hoa/game/screens/PlayScreen.java
+++ HoA/core/src/com/hoa/game/screens/PlayScreen.java
@@ -52,8 +52,9 @@ public class PlayScreen implements Screen {
     public void handleInput(float dt){
 
         if (Gdx.input.isTouched()){
-            gamecam.position.x =+ 100 * dt;
+            gamecam.position.y += 2;
         }
+
     }
 
     public void update(float dt){
@@ -71,7 +72,8 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        update(delta);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.render();
 
