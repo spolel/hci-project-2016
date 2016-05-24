7a11081ec9f3143647ef84852d9de170523d05cb
JeanPaulCaratti <polithunder.94@gmail.com>
2016-05-24 12:24:38 +0200

    fixed graphic issue !!!must remove when testing on the beamer!!!

--- hoa/desktop/src/com/hoa/game/desktop/DesktopLauncher.java
+++ hoa/desktop/src/com/hoa/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.hoa.game.HoA;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useHDPI = true;
 		new LwjglApplication(new HoA(), config);
 	}
 }
